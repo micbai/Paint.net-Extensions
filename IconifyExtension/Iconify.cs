70733a18a6662dfa85bbee4b99eccda538deb066
Michael Baier <michael.baier@live.com>
2024-03-10 19:23:25 +0100

    First working draft

--- IconifyExtension/Iconify.cs
+++ IconifyExtension/Iconify.cs
@@ -33,6 +33,8 @@ using RadioButtonControl = System.Byte;
 using MultiLineTextboxControl = System.String;
 using LabelComment = System.String;
 using LayerControl = System.Int32;
+using PaintDotNet.Imaging;
+using System.Drawing.Imaging;
 
 [assembly: AssemblyTitle("Iconify plugin for Paint.NET")]
 [assembly: AssemblyDescription("Iconify FileType")]
@@ -49,6 +51,17 @@ using LayerControl = System.Int32;
 
 namespace IconifyFileType
 {
+    public enum ImageSize
+    {
+        IconAuto,
+        Icon_16x16 = 16,
+        Icon_32x32 = 32,
+        Icon_48x48 = 48,
+        Icon_64x64 = 64,
+        Icon_128x128 = 128,
+        Icon_256x256 = 256
+    }
+
     public sealed class IconifyFactory : IFileTypeFactory
     {
         public FileType[] GetFileTypeInstances()
@@ -69,6 +82,8 @@ namespace IconifyFileType
     [PluginSupportInfo<IconifySupportInfo>(DisplayName = "Iconify")]
     internal class IconifyPlugin : PropertyBasedFileType
     {
+        public const string ImageSizeString = "Image Size";
+
         internal IconifyPlugin()
             : base(
                 "Iconify",
@@ -83,10 +98,6 @@ namespace IconifyFileType
             RandomNumberInstanceSeed = unchecked((uint)DateTime.Now.Ticks);
         }
 
-        public enum PropertyNames
-        {
-            Amount1
-        }
 
         #region Random Number Support
         private readonly uint RandomNumberInstanceSeed;
@@ -168,27 +179,19 @@ namespace IconifyFileType
         public override PropertyCollection OnCreateSavePropertyCollection()
         {
             List<Property> props = new List<Property>();
-
-            props.Add(new BooleanProperty(PropertyNames.Amount1, false));
-
+            props.Add(StaticListChoiceProperty.CreateForEnum(ImageSizeString, ImageSize.IconAuto));
             return new PropertyCollection(props);
         }
 
         public override ControlInfo OnCreateSaveConfigUI(PropertyCollection props)
         {
-            ControlInfo configUI = CreateDefaultSaveConfigUI(props);
+            ControlInfo config = CreateDefaultSaveConfigUI(props);
 
-            configUI.SetPropertyControlValue(PropertyNames.Amount1, ControlInfoPropertyNames.DisplayName, string.Empty);
-            configUI.SetPropertyControlValue(PropertyNames.Amount1, ControlInfoPropertyNames.Description, "Checkbox Description");
-            configUI.SetPropertyControlValue(PropertyNames.Amount1, ControlInfoPropertyNames.ShowHeaderLine, false);
-
-            return configUI;
+            return config;
         }
 
         protected override void OnSaveT(Document input, Stream output, PropertyBasedSaveConfigToken token, Surface scratchSurface, ProgressEventHandler progressCallback)
         {
-            Amount1 = token.GetProperty<BooleanProperty>(PropertyNames.Amount1).Value;
-
             SaveImage(input, output, token, scratchSurface, progressCallback);
         }
 
@@ -196,6 +199,7 @@ namespace IconifyFileType
         {
             return LoadImage(input);
         }
+
         #region User Entered Code
         // Name:
         // Author:
@@ -205,9 +209,6 @@ namespace IconifyFileType
         // LoadExtns: .foo, .bar
         // SaveExtns: .foo, .bar
         // Flattened: false
-        #region UICode
-        CheckboxControl Amount1 = false; // Checkbox Description
-        #endregion
 
         private const string HeaderSignature = ".PDN";
 
@@ -216,78 +217,87 @@ namespace IconifyFileType
             // Render a flattened view of the Document to the scratch surface.
             input.CreateRenderer().Render(scratchSurface);
 
-            if (Amount1)
+            // create a new bitmap
+            Bitmap bmp = new Bitmap(scratchSurface.Width, scratchSurface.Height);
+            for (int y = 0; y < scratchSurface.Height; y++)
             {
-                new UnaryPixelOps.Invert().Apply(scratchSurface, scratchSurface.Bounds);
-            }
-
-            // The stream paint.net hands us must not be closed.
-            using (BinaryWriter writer = new BinaryWriter(output, Encoding.UTF8, leaveOpen: true))
-            {
-                // Write the file header.
-                writer.Write(Encoding.ASCII.GetBytes(HeaderSignature));
-                writer.Write(scratchSurface.Width);
-                writer.Write(scratchSurface.Height);
-
-                for (int y = 0; y < scratchSurface.Height; y++)
+                for (int x = 0; x < scratchSurface.Width; x++)
                 {
-                    // Report progress if the callback is not null.
-                    if (progressCallback != null)
-                    {
-                        double percent = (double)y / scratchSurface.Height;
-
-                        progressCallback(null, new ProgressEventArgs(percent));
-                    }
-
-                    for (int x = 0; x < scratchSurface.Width; x++)
-                    {
-                        // Write the pixel values.
-                        ColorBgra color = scratchSurface[x, y];
-
-                        writer.Write(color.Bgra);
-                    }
+                    // Read the pixel values from the bitmap
+                    bmp.SetPixel(x, y, scratchSurface[x, y]);
                 }
             }
+
+            ImageSize imageSize = (ImageSize)token.GetProperty(ImageSizeString).Value;
+            int pixelSize = imageSize switch
+            {
+                ImageSize.IconAuto or ImageSize.Icon_32x32 => 32,
+                ImageSize.Icon_16x16 => 16,
+                ImageSize.Icon_48x48 => 48,
+                ImageSize.Icon_64x64 => 64,
+                ImageSize.Icon_128x128 => 128,
+                ImageSize.Icon_256x256 => 256,
+                _ => 32
+            };
+
+            // Create icon
+            ConvertToIco(bmp, pixelSize, output);
         }
+
+        /// <summary>
+        /// https://stackoverflow.com/questions/17212704/convert-image-to-icon-in-c-sharp
+        /// </summary>
+        /// <param name="img"></param>
+        /// <param name="size"></param>
+        /// <param name="outStream"></param>
+        private void ConvertToIco(Bitmap img, int size, Stream outStream)
+        {
+            var bmp = new Bitmap(img, size, size);
+            var bw = new BinaryWriter(outStream);
+            using (var msImg = new MemoryStream())
+            {
+                img.Save(msImg, ImageFormat.Png);
+                bw.Write((short)0);           //0-1 reserved
+                bw.Write((short)1);           //2-3 image type, 1 = icon, 2 = cursor
+                bw.Write((short)1);           //4-5 number of images
+                bw.Write((byte)size);         //6 image width
+                bw.Write((byte)size);         //7 image height
+                bw.Write((byte)0);            //8 number of colors
+                bw.Write((byte)0);            //9 reserved
+                bw.Write((short)0);           //10-11 color planes
+                bw.Write((short)32);          //12-13 bits per pixel
+                bw.Write((int)msImg.Length);  //14-17 size of image data
+                bw.Write(22);                 //18-21 offset of image data
+                bw.Write(msImg.ToArray());    // write image data
+                bw.Flush();
+                bw.Seek(0, SeekOrigin.Begin);
+            }
+        }
+
 
         Document LoadImage(Stream input)
         {
             Document doc = null;
 
-            // The stream paint.net hands us must not be closed.
-            using (BinaryReader reader = new BinaryReader(input, Encoding.UTF8, leaveOpen: true))
+            Icon icon = new Icon(input);
+            Bitmap bmp = icon.ToBitmap();
+
+            doc = new Document(bmp.Width, bmp.Height);
+
+            // Create a background layer.
+            BitmapLayer layer = Layer.CreateBackgroundLayer(bmp.Width, bmp.Height);
+
+            for (int y = 0; y < bmp.Height; y++)
             {
-                // Read and validate the file header.
-                byte[] headerSignature = reader.ReadBytes(4);
-
-                if (Encoding.ASCII.GetString(headerSignature) != HeaderSignature)
+                for (int x = 0; x < bmp.Width; x++)
                 {
-                    throw new FormatException("Invalid file signature.");
+                    // Read the pixel values from the bitmap
+                    layer.Surface[x, y] = bmp.GetPixel(x, y);
                 }
-
-                int width = reader.ReadInt32();
-                int height = reader.ReadInt32();
-
-                // Create a new Document.
-                doc = new Document(width, height);
-
-                // Create a background layer.
-                BitmapLayer layer = Layer.CreateBackgroundLayer(width, height);
-
-                for (int y = 0; y < height; y++)
-                {
-                    for (int x = 0; x < width; x++)
-                    {
-                        // Read the pixel values from the file.
-                        uint bgraColor = reader.ReadUInt32();
-
-                        layer.Surface[x, y] = ColorBgra.FromUInt32(bgraColor);
-                    }
-                }
-
-                // Add the new layer to the Document.
-                doc.Layers.Add(layer);
             }
+
+            // Add the new layer to the Document.
+            doc.Layers.Add(layer);
 
             return doc;
         }
